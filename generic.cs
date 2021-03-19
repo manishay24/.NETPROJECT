78b36f9205a331c803a267908b906920087dbfd8
manishay24 <75357850+manishay24@users.noreply.github.com>
2021-03-19 16:37:24 +0530

    Add files via upload

---
+++ generic.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace GenericApp
+{
+    class Program
+    {
+        //Generic method  
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {  
+            int a = 40, b = 60;
+            Console.WriteLine("Before swap: {0}, {1}", a, b);
+
+            Swap<int>(ref a, ref b);
+
+            Console.WriteLine("After swap: {0}, {1}", a, b);
+
+            Console.ReadLine();
+        }
+    }
+}
+
