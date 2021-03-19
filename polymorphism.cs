b51166d0bd41b858bff122c5d7993694ea904442
manishay24 <75357850+manishay24@users.noreply.github.com>
2021-03-19 16:22:29 +0530

    Add files via upload

---
+++ polymorphism.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+namespace oops
+{
+    public abstract class Employee
+    {
+        public virtual void LeaderName()
+        {
+        }
+    }
+
+    public class hrDepart : Employee
+    {
+        public override void LeaderName()
+        {
+            Console.WriteLine("Mr. jone");
+        }
+    }
+    public class itDepart : Employee
+    {
+        public override void LeaderName()
+        {
+            Console.WriteLine("Mr. Tom");
+        }
+    }
+
+    public class financeDepart : Employee
+    {
+        public override void LeaderName()
+        {
+            Console.WriteLine("Mr. Linus");
+        }
+    }
+
+    class PolymorphismDemo
+    {
+        static void Main(string[] args)
+        {
+            hrDepart obj1 = new hrDepart();
+            itDepart obj2 = new itDepart();
+            financeDepart obj3 = new financeDepart();
+
+            obj1.LeaderName();
+            obj2.LeaderName();
+            obj3.LeaderName();
+
+            Console.ReadKey();
+        }
+    }
+}
+
